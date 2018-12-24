4bb501f481d36669b4ad33c5af931b3f9d45bb71
dongxinghuashao <dongxinghuashao@tutanota.com>
2018-12-24 16:53:47 +0800

    while loop

---
+++ while.py
@@ -0,0 +1,19 @@
+# while True:
+#     print("milesfashion")
+#     break
+
+# while 10<11:
+#     print("milesfashion")
+#     break
+#
+# condition =1
+# while condition<=10:
+#     print(condition)
+#     condition=condition+1
+# print(" ")
+# for i in range(1,11):
+#      print(i)
+while True:
+    a = int(input("please enter number a :"))
+    b = int(input("please enter number b: "))
+    print("a+b= " + str(a + b))
