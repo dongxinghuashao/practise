7c6fb779ecb7479b5e32b8043c9ee1f1d2984ac2
dongxinghuashao <dongxinghuashao@tutanota.com>
2018-12-24 17:34:35 +0800

    def function();
        return()

---
+++ Function.py
@@ -0,0 +1,33 @@
+# # define 下定义
+# def Function_name():
+
+# def func1():
+#     print("milesfashion")
+#
+#
+# func1()
+# a = 10
+# b = 10
+# print(a + b)
+# c = 20
+# d = 20
+# # print(c + d)
+#
+# def sum(num1, num2):
+#     print(num1 + num2)
+# sum(1, 2)
+# sum(10, 10)
+# sum(20, 20)
+
+def func2():
+    return ("hello milesfashion")
+
+
+def sum2(a, b):
+    return (a + b)
+
+
+a = sum2(1, 2)
+print(a)
+b = sum2(3, 3)
+print(b)
