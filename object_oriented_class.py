0e1b2fc7a437b798d88e42c6dc0f5c3e8cbbd138
dongxinghuashao <dongxinghuashao@tutanota.com>
2018-12-26 14:09:55 +0800

    def converter(weight):     ponds=weight/0.45     print(ponds) converter(200)

---
+++ object_oriented_class.py
@@ -0,0 +1,19 @@
+# object oriented
+
+# class Student():
+#     def eat(self):
+#         print("milesfashion can eat")
+#     def run(self):
+#         print("milesfashion can run")
+# miles=Student()
+# miles.run()
+#
+# friut="apple"
+# print(friut.upper())
+
+class Student:
+    def eat(self, name, age):
+        print(name + " can eat" + " and " + "age is " + age)
+
+
+Student().eat("milesfashion", "18")
