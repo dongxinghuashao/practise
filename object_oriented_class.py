43782920ec792b26cbeb97243e6030e84e48cc7d
dongxinghuashao <dongxinghuashao@tutanota.com>
2018-12-27 14:59:51 +0800

    def converter(weight):     ponds=weight/0.45     print(ponds) converter(200)

--- object_oriented_class.py
+++ object_oriented_class.py
@@ -11,9 +11,32 @@
 # friut="apple"
 # print(friut.upper())
 
-class Student:
-    def eat(self, name, age):
-        print(name + " can eat" + " and " + "age is " + age)
+# class Student:
+#     def eat(self, name, age):
+#         print(name + " can eat" + " and " + "age is " + age)
+#
+#
+# Student().eat("milesfashion", "18")
+
+class Student():
+    name = "milesfahion"
+    age = 20
+
+    def eat(self):
+        print(self.name, "can eat", " and age is", self.age)
+
+    @staticmethod
+    def run():
+        print("you can run")
+
+    def speak(self):
+        print(self.name,"can speaking")
 
 
-Student().eat("milesfashion", "18")
+
+student1 = Student()
+student1.run()
+student2 = Student()
+student2.eat()
+student3=Student()
+student3.speak()
